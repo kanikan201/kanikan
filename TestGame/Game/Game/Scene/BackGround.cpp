4ee32c20546375aa7da22d35bb2cdaeac71018ee
Thyodo <a>
2017-06-28 15:47:13 +0900

    背景調整

--- TestGame/Game/Game/Scene/BackGround.cpp
+++ TestGame/Game/Game/Scene/BackGround.cpp
@@ -32,12 +32,8 @@ bool BackGround::Start()
 	skinModel.SetLight(&g_gameScene->getLight());			//デフォルトライトを設定。
 	animation.PlayAnimation(AnimationRun);
 
-
-	/*qRotX.SetRotation(CVector3(1.0f, 0.0f, 0.0f), CMath::DegToRad(-90.0f));
-	qRotY.SetRotation(CVector3(0.0f, 1.0f, 0.0f), CMath::DegToRad(20.0f));
-	View.Multiply(qRotX, qRotY);*/
-
-	camera.SetPosition({0.0f, 0.0f, 1.2f});
+	//背景のユニティちゃん用のカメラの設定
+	camera.SetPosition({0.0f, 0.0f, 1.6f});
 	camera.SetTarget({0.0f, 0.0f, 0.0f});
 	camera.Update();
 	return true;
@@ -53,22 +49,7 @@ void BackGround::Update()
 	//アニメーション更新
 	animation.Update(1.0f / 30.0f);
 
-	//ステージによって背景のユニティちゃんの位置を変更
-	switch (g_gameScene->GetStage())
-	{
-	case 0: {
-		static CVector3 pos = { 0.7f, -0.45f, 0.2f };
-		skinModel.Update(pos, CQuaternion::Identity, CVector3(1.0f, 1.0f, 1.0f));
-	}break;
-	case 1:
-		skinModel.Update(CVector3(70.0f, 0.0f, -10.0f), View, CVector3(50.0f, 50.0f, 50.0f));
-		break;
-	case 2:
-		skinModel.Update(CVector3(50.0f, 0.0f, 20.0f), View, CVector3(50.0f, 50.0f, 50.0f));
-		break;
-	default:
-		break;
-	}
+	skinModel.Update(CVector3 (0.7f, -0.45f, 0.18f), CQuaternion::Identity, CVector3(1.0f, 1.0f, 1.0f));
 
 }
 
